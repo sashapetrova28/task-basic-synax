4d440b44e2628d83aa240e1ef751c9067b0b2e23
sashapetrova28 <sasha_281200@mail.ru>
2022-03-28 23:31:51 +0500

    task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,23 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    const roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
+    for (let j = 0; j < str.length; j++) {
+        if (roman[str[j]] < roman[str[j + 1]]) {
+            result = result - roman[str[j]];
+        } else {
+            result = result + roman[str[j]];
+        }
+    }
+
     return result;
 }
